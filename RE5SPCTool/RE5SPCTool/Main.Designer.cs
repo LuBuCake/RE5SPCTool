df43bb870e163eef28aefd442196b5bf543b49b3
Lucas <madneshell@outlook.com>
2020-11-06 21:20:47 -0300

    Update Main.Designer.cs

--- RE5SPCTool/RE5SPCTool/Main.Designer.cs
+++ RE5SPCTool/RE5SPCTool/Main.Designer.cs
@@ -1,4 +1,21 @@
-﻿namespace RE5SPCTool
+/*
+This file is part of RESIDENT EVIL 5 FWSE/SPC Tool.
+
+RESIDENT EVIL 5 FWSE/SPC Tool is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+RESIDENT EVIL 5 FWSE/SPC Tool is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RESIDENT EVIL 5 FWSE/SPC Tool. If not, see <https://www.gnu.org/licenses/>6.
+*/
+
+namespace RE5SPCTool
 {
     partial class Main
     {
